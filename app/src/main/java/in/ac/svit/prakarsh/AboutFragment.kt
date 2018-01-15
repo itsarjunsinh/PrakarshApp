8189e3c013c5737686a160c9c298bf91d14c40ef
itsarjunsinh <mrarjunsinh@gmail.com>
2018-01-15 12:58:06 +0530

    Implement JSON fetching using Volley in AboutFragment

--- app/src/main/java/in/ac/svit/prakarsh/AboutFragment.kt
+++ app/src/main/java/in/ac/svit/prakarsh/AboutFragment.kt
@@ -3,9 +3,14 @@ package `in`.ac.svit.prakarsh
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.fragment_about.*
 
 
@@ -22,5 +27,25 @@ class AboutFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        updateViewsFromJson()
+    }
+
+    private fun updateViewsFromJson() {
+        try {
+            var url: String? = context?.getString(R.string.url_about)
+            val que = Volley.newRequestQueue(context)
+            val req = JsonObjectRequest(Request.Method.GET,url,null,
+                    Response.Listener {
+                        response ->
+                        Log.d(javaClass.name,"JSON Successfully fetched")
+                        about_txt_description.text=response["about"].toString()
+                    }, Response.ErrorListener {
+                error ->
+                Log.d(javaClass.name,"Volley Response Error Occurred, URL: $url Error: ${error.message}")
+            })
+            que.add(req)
+        }catch (e: Exception){
+            Log.d(javaClass.name,"Exception caught during Volley Request.")
+        }
     }
 }
