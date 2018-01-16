e10598f73e18b2044a920984dec0ae37546faf13
itsarjunsinh <mrarjunsinh@gmail.com>
2018-01-16 14:49:34 +0530

    Create events subcategory activity

---
+++ app/src/main/java/in/ac/svit/prakarsh/EventsSubcategoryActivity.kt
@@ -0,0 +1,17 @@
+package `in`.ac.svit.prakarsh
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_team_category.*
+
+/**
+ * Created by itsarjunsinh on 1/16/18.
+ */
+class EventsSubcategoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team_category)
+
+    }
+}
