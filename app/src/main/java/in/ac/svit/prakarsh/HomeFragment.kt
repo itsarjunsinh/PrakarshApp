a19003ef0695b5310c2e62ee994619b881b93867
itsarjunsinh <mrarjunsinh@gmail.com>
2018-01-27 16:53:55 +0530

    Add recyclerView for featured speakers

--- app/src/main/java/in/ac/svit/prakarsh/HomeFragment.kt
+++ app/src/main/java/in/ac/svit/prakarsh/HomeFragment.kt
@@ -1,8 +1,12 @@
 package `in`.ac.svit.prakarsh
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +15,10 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.item_featured_speakers.view.*
+import org.json.JSONArray
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Created by itsarjunsinh on 1/11/18.
@@ -31,8 +38,10 @@ class HomeFragment : Fragment() {
     }
 
     private fun updateViewsFromJson() {
-        var url: String? = context?.getString(R.string.url_main)
-        val req = JsonObjectRequest(Request.Method.GET, url, null,
+
+        //Timer section
+        var url = context?.getString(R.string.url_main)
+        var req = JsonObjectRequest(Request.Method.GET, url, null,
                 Response.Listener { response ->
                     Log.d(javaClass.name, "JSON Successfully fetched")
 
@@ -88,6 +97,58 @@ class HomeFragment : Fragment() {
         })
 
         VolleySingleton.getInstance(context?.applicationContext).requestQueue.add(req)
+
+        //Featured events section
+        url = context?.getString(R.string.url_featured)
+        req = JsonObjectRequest(Request.Method.GET, url, null,
+                Response.Listener { response ->
+                    Log.d(javaClass.name, "JSON Successfully fetched")
+
+                    val speakerList: ArrayList<Speaker> = ArrayList()
+                    var featuredSpeakers = JSONArray()
+
+                    if(response.has("featuredSpeakers")){
+                        featuredSpeakers = response.getJSONArray("featuredSpeakers")
+                    }
+
+                    for(i in 0..(featuredSpeakers.length() - 1)){
+
+                        var name = ""
+                        if (featuredSpeakers.getJSONObject(i).has("name")) {
+                            name = featuredSpeakers.getJSONObject(i).getString("name")
+                        }
+
+                        var shortDescription = ""
+                        if (featuredSpeakers.getJSONObject(i).has("shortDescription")) {
+                            shortDescription = featuredSpeakers.getJSONObject(i).getString("shortDescription")
+                        }
+
+                        var description = ""
+                        if (featuredSpeakers.getJSONObject(i).has("description")) {
+                            description = featuredSpeakers.getJSONObject(i).getString("description")
+                        }
+
+                        var imageUrl = ""
+                        if (featuredSpeakers.getJSONObject(i).has("imageUrl")) {
+                            imageUrl = featuredSpeakers.getJSONObject(i).getString("imageUrl")
+                        }
+
+                        var dataUrl = ""
+                        if (featuredSpeakers.getJSONObject(i).has("dataUrl")) {
+                            dataUrl = featuredSpeakers.getJSONObject(i).getString("dataUrl")
+                        }
+
+                        speakerList.add(Speaker(name, shortDescription, description, imageUrl, dataUrl))
+                    }
+
+                    home_rv_featured_speakers?.layoutManager = LinearLayoutManager(context)
+                    home_rv_featured_speakers?.adapter = SpeakerRecyclerAdapter(context, speakerList)
+
+                }, Response.ErrorListener { error ->
+            Log.d(javaClass.name, "Volley Response Error Occurred, URL: $url Error: ${error.message}")
+        })
+
+        VolleySingleton.getInstance(context?.applicationContext).requestQueue.add(req.setShouldCache(false))
     }
 
     private fun startCountdown(eventDate: Calendar) {
@@ -129,5 +190,37 @@ class HomeFragment : Fragment() {
 
         remainingText += String.format("%02d Hours : %02d Minutes: %02d Seconds", remainingHours, remainingMinutes, remainingSeconds)
         return remainingText
+    }
+
+    private class Speaker(val name: String,val shortDescription: String, val description: String, val imageUrl: String, val dataUrl: String)
+
+    private class SpeakerRecyclerAdapter(private val context: Context?, private val speakerList: ArrayList<Speaker>): RecyclerView.Adapter<SpeakerRecyclerAdapter.CustomViewHolder>() {
+
+        class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return speakerList.size
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CustomViewHolder {
+            val layoutInflater = LayoutInflater.from(parent?.context)
+            val cellForRow = layoutInflater.inflate(R.layout.item_featured_speakers, parent, false)
+            return CustomViewHolder(cellForRow)
+        }
+
+        override fun onBindViewHolder(holder: CustomViewHolder?, position: Int) {
+            holder?.view?.featured_speakers_txt_name?.text = speakerList[position].name
+            holder?.view?.featured_speakers_txt_short_description?.text = speakerList[position].shortDescription
+            holder?.view?.featured_speakers_txt_description?.text = speakerList[position].description
+            holder?.view?.featured_speakers_img_speaker?.setDefaultImageResId(R.drawable.ic_image_black)
+            holder?.view?.featured_speakers_img_speaker?.setErrorImageResId(R.drawable.ic_broken_image_black)
+            holder?.view?.featured_speakers_img_speaker?.setImageUrl(speakerList[position].imageUrl,VolleySingleton.getInstance(context).imageLoader)
+            holder?.view?.setOnClickListener{
+                Log.d(javaClass.name,"${speakerList[position].name} Clicked")
+                var intent = Intent(context, EventInfoActivity::class.java)
+                intent.putExtra("url", speakerList[position].dataUrl)
+                context?.startActivity(intent)
+            }
+        }
     }
 }
