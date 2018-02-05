beb5f5ff8886f8287fdf91286604b5ca2d4f0e3a
itsarjunsinh <mrarjunsinh@gmail.com>
2018-02-05 09:36:25 +0530

    Disable bottom navigation shift mode

--- app/src/main/java/in/ac/svit/prakarsh/MainActivity.kt
+++ app/src/main/java/in/ac/svit/prakarsh/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
+import android.support.design.internal.BottomNavigationItemView
+import android.support.design.internal.BottomNavigationMenuView
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +17,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        main_navigation.selectedItemId=action_home
+        disableNavigationShiftMode(main_navigation)
+        main_navigation?.selectedItemId = action_home
         startFragmentTransaction(HomeFragment())
-        main_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        main_navigation?.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -44,5 +49,28 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
                 .replace(container.id, fragment)
                 .commit()
+    }
+
+    private fun disableNavigationShiftMode(view: BottomNavigationView?) {
+        val menuView = view?.getChildAt(0) as BottomNavigationMenuView
+        try {
+            val shiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
+            shiftingMode.isAccessible = true
+            shiftingMode.setBoolean(menuView, false)
+            shiftingMode.isAccessible = false
+            for (i in 0 until menuView.childCount) {
+                val item = menuView.getChildAt(i) as BottomNavigationItemView
+
+                item.setShiftingMode(false)
+                // set once again checked value, so view will be updated
+
+                item.setChecked(item.itemData.isChecked)
+            }
+        } catch (e: NoSuchFieldException) {
+            Log.e(javaClass.name, "Unable to get shift mode field", e)
+        } catch (e: IllegalAccessException) {
+            Log.e(javaClass.name, "Unable to change value of shift mode", e)
+        }
+
     }
 }
