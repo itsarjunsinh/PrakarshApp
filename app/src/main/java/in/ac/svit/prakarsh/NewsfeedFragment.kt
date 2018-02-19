2567586fa8e29c5e0cd5341f8ddbf2364b6a07d6
Arjunsinh Jadeja <mrarjunsinh@gmail.com>
2018-02-20 04:39:36 +0530

    Create newsfeed fragment class

---
+++ app/src/main/java/in/ac/svit/prakarsh/NewsfeedFragment.kt
@@ -0,0 +1,23 @@
+package `in`.ac.svit.prakarsh
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by arjun on 20-02-2018.
+ */
+class NewsfeedFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_newsfeed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
